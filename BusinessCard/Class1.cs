6592e2c11a94fd3360858ad22378b7b0702cd092
Volodya Mykhailiuk <volodymyr.mykhailiuk.pm.2012@lpnu.ua>
2017-05-21 21:16:55 +0300

    Adding class

---
+++ BusinessCard/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCard
+{
+    public class BusinessCard
+    {
+        public string name = "";
+        public BusinessCard(string name)
+        {
+            this.name = name;
+        }
+        public string FindOutWhose()
+        {
+            return name + "'s business card";
+        }
+    }
+}

e5ec805778dd302ab9111b5edc6d3d0bee46fef1
Volodya Mykhailiuk <volodymyr.mykhailiuk.pm.2012@lpnu.ua>
2017-05-21 20:43:46 +0300

    Adding class CreditCard

---
+++ CreditCard/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditCard
+{
+    public class CreditCard
+    {
+        public decimal amount = 0;
+        private string msg = "Credit Card balanse: ";
+
+        public string GetSumCreditCard()
+        {
+            return msg + amount.ToString();
+        }
+
+        public CreditCard(decimal amount)
+        {
+            this.amount = amount;
+        }
+    }
+}
--- Purse/Program.cs
+++ Purse/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Money;
+using CreditCard;
 
 namespace Purse
 {

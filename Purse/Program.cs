bf1a3e5a08dae08a86ece66908af9b5fe452d7a4
Volodya Mykhailiuk <volodymyr.mykhailiuk.pm.2012@lpnu.ua>
2017-05-22 19:12:35 +0300

    new commit

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -20,6 +20,8 @@ namespace Purse
             var bcard = new BusinessCard.BusinessCard("Vova");
             Console.WriteLine(bcard.FindOutWhose());
             Console.WriteLine("This is working");
+            Console.WriteLine(bcard.FindOutWhose());
+            Console.WriteLine("This is working");
         }
     }
 }

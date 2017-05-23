9ceb26d417b7d6cf980ebc1257fb9c48e00d79de
Volodya Mykhailiuk <volodymyr.mykhailiuk.pm.2012@lpnu.ua>
2017-05-23 16:30:40 +0300

    new commit 2

--- Purse/Program.cs
+++ Purse/Program.cs
@@ -22,7 +22,7 @@ namespace Purse
             Console.WriteLine("This is working");
             Console.WriteLine(bcard.FindOutWhose());
             Console.WriteLine("This is working");
-
+            Console.WriteLine(bcard.FindOutWhose());
             var money = new Money.Money(1000);
             
 
